988d69f1442a22d64d1e2ea82aad489487e66c64
Luís Felipe Santos Oliva <luis.oliva@aluno.faculdadeimpacta.com.br>
2026-04-10 14:30:19 -0300

    nova feature com erro

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ def somar(a: int, b: int):
     # Apenas retorna a soma dos dois números
     return {"resultado": a + b}
 
+@app.get("/multiplicar/{a}/{b}")
+def multiplicar(a: int, b: int):
+    return {"resultado": a * b}
+
---
+++ teste_main.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"mensagem": "API funcionando!"}
+
+def test_somar():
+    response = client.get("/somar/5/3")
+    assert response.status_code == 200
+    assert response.json() == {"resultado": 8}
+
+def test_multiplicar():
+    response = client.get("/multiplicar/2/2")
+    assert response.status_code == 200
+    assert response.json() == {"resultado": 5}  # ERRADO
