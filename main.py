1f433a231499255af1a3dd20b9725817e0332479
Luís Felipe Santos Oliva <luis.oliva@aluno.faculdadeimpacta.com.br>
2026-04-10 14:08:18 -0300

    projeto inicial

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"mensagem": "API funcionando!"}
+
+@app.get("/somar/{a}/{b}")
+def somar(a: int, b: int):
+    # Apenas retorna a soma dos dois números
+    return {"resultado": a + b}
+
